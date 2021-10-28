f3b29548e97c5b6459ffbe8d39ae5fc82774a888
王永帅 <wangyongshuai.ayu@bytedance.com>
2021-10-28 21:26:47 +0800

    feat: 添加地图

--- src/scene/PlayScene.ts
+++ src/scene/PlayScene.ts
@@ -19,46 +19,6 @@ export class PlayScene extends Phaser.Scene {
 
     }
     preload() {
-        // this.anims.create({
-        //     key: "dazzle",
-        //     frameRate: 10,
-        //     frames: this.anims.generateFrameNames("daze", {
-        //         prefix: 'daze0',
-        //         suffix: '.png',
-        //         start: 0,
-        //         end: 41
-        //     }),
-        //     repeat: -1
-        // })
-        // this.textures.addSpriteSheetFromAtlas("hooded", {
-        //     frameWidth: 64,
-        //     frameHeight: 64,
-        //     atlas: 'characters',
-        //     frame: 'hooded'
-        // })
-        // this.textures.addSpriteSheetFromAtlas("mandy", {
-        //     frameWidth: 64,
-        //     frameHeight: 64,
-        //     atlas: 'characters',
-        //     frame: 'mandy'
-        // })
-        // this.anims.create({
-        //     key: 'right',
-        //     frameRate: 10,
-        //     frames: this.anims.generateFrameNumbers('hooded', {
-        //         frames: [143, 144, 145, 146, 147, 148, 149, 150, 151]
-        //     }),
-        //     repeat: -1
-        // })
-        // this.anims.create({
-        //     key: 'left',
-        //     frameRate: 10,
-        //     frames: this.anims.generateFrameNumbers('mandy', {
-        //         frames: [143, 144, 145, 146, 147, 148, 149, 150, 151]
-        //     }),
-        //     repeat: -1
-        // })
-        
         // anna的四个方向的运动
         this.anims.create({
             key: 'left',
@@ -113,6 +73,12 @@ export class PlayScene extends Phaser.Scene {
             frame: 'hooded'
         })
 
+        // 在该游戏场景加载游戏地图
+        this.load.image('terrain', 'src/assets/image/terrain_atlas.png')
+        this.load.image('items', 'src/assets/image/items.png')
+
+        this.load.tilemapTiledJSON('mappy', 'src/assets/maps/mappy.json')
+
     }
     create() {
         // let cat = this.add.sprite(100, 100, CST.SPRITE.CAT).setScale(2)
@@ -121,7 +87,7 @@ export class PlayScene extends Phaser.Scene {
         this.anna = new CharacterSprite(this, 400, 400, 'anna', 26)
         // this.hooded = this.physics.add.sprite(200, 200, 'hooded', 26).setScale(2).setImmovable(true) // 设置不可移动的
         this.hooded = new CharacterSprite(this, 200, 200, 'hooded', 26)
-        
+
         this.fireAttack = this.physics.add.group()
         this.assassins = this.physics.add.group({ immovable: true })
         this.assassins.add(this.hooded)
@@ -140,7 +106,7 @@ export class PlayScene extends Phaser.Scene {
         this.keyboard = this.input.keyboard.addKeys("w, a, s, d");
         this.input.on('pointermove', (pointer: Phaser.Input.Pointer) => {
             if (pointer.isDown) {
-                let fire = this.add.sprite(pointer.x, pointer.y, 'daze', 'fire00.png').play('blaze')
+                let fire = this.add.sprite(pointer.worldX, pointer.worldY, 'daze', 'fire00.png').play('blaze')
                 this.fireAttack.add(fire)
                 fire.on('animationcomplete', () => {
                     fire.destroy()
@@ -165,6 +131,49 @@ export class PlayScene extends Phaser.Scene {
             for (let i = 0; i < 2; i++) {
                 this.assassins.add(this.physics.add.sprite(x, y, 'hooded', 26).setScale(2).setImmovable(true)) // 设置不可移动的
             }
+        })
+        let mappy = this.add.tilemap('mappy') // 添加json文件
+        let terrain = mappy.addTilesetImage('terrain_atlas', 'terrain') // 为json添加对应的图片
+        let item = mappy.addTilesetImage('items')
+        // 添加图层
+        let botLayer = mappy.createLayer("bot", [terrain], 0, 0).setDepth(-1)
+        let topLayer = mappy.createLayer("top", [terrain], 0, 0).setDepth(2)
+        this.physics.add.collider(this.anna, topLayer)
+        topLayer.setCollisionByProperty({ collides: true }) // 通过属性来设置碰撞
+        topLayer.setCollision([269, 270, 271, 301, 302, 303, 333, 334, 335]) // 通过index来设置
+        
+        // 碰撞回调
+        topLayer.setTileLocationCallback(10, 8, 1, 1, () => {
+            alert('卧槽！！')
+            // @ts-ignore
+            topLayer.setTileLocationCallback(10, 8, 1, 1, null)
+        }) //通过地图位置类获得碰撞回调
+        // @ts-ignore
+        topLayer.setTileIndexCallback([269, 270, 271, 301, 302, 303, 333, 334, 335], () => {
+            alert('这咋有个洞？？')
+            // @ts-ignore
+            topLayer.setTileIndexCallback([269, 270, 271, 301, 302, 303, 333, 334, 335], null)
+        })
+
+        let items = mappy.createFromObjects('pickup', { key: CST.SPRITE.CAT }).map((sprite: Phaser.GameObjects.Sprite) => {
+            sprite.setScale(2)
+            sprite.setInteractive() // 可交互的
+        })
+        this.input.on('gameobjectdown', (pointer: Phaser.Input.Pointer, obj: Phaser.GameObjects.Sprite) => {
+            obj.destroy()
+        })
+        this.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
+            let tile = mappy.getTileAt(mappy.worldToTileX(pointer.x), mappy.worldToTileY(pointer.y)) //获取到鼠标点击位置的tile元素
+            console.log(tile)
+        })
+
+        this.cameras.main.startFollow(this.anna)
+        this.physics.world.setBounds(0, 0, mappy.widthInPixels, mappy.heightInPixels)
+
+        topLayer.renderDebug(this.add.graphics(), {
+            tileColor: null,
+            collidingTileColor: new Phaser.Display.Color(243, 134, 122, 200),
+            faceColor: new Phaser.Display.Color(243, 134, 122, 200)
         })
     }
     update(time: number, delta: number) {
